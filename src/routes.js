ba2269a80f5da6ff4fecf95465ead259337a5993
Frederiko C. M. Ribeiro <frederiko.cmr@gmail.com>
2019-09-21 20:06:44 -0300

    Added Sucrase and Nodemon

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+import UserDetailsController from './app/controllers/UserDetailsController';
+import UserRepositoriesController from './app/controllers/UserRepositoriesController';
+
 const routes = new Router();
+const commonUrl = 'api/users';
 
-routes.post('/users', UserController.index);
+routes.post(commonUrl, UserController.index);
 
-routes.post('/users', UserDetailsController.index);
+routes.post(`${commonUrl}/:username/details`, UserDetailsController.index);
 
-routes.post('/users', UserRepositoriesController.index);
+routes.post(`${commonUrl}/:username/repos`, UserRepositoriesController.index);
 
-export default routes;
+export default routes; 
