babb7a0c9aac0c3f161086f98a4966abd80634f7
Frederiko C. M. Ribeiro <frederiko.cmr@gmail.com>
2019-09-21 21:13:22 -0300

    Updated Users route controller

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,12 @@ import UserDetailsController from './app/controllers/UserDetailsController';
 import UserRepositoriesController from './app/controllers/UserRepositoriesController';
 
 const routes = new Router();
-const commonUrl = 'api/users';
+const commonUrl = '/api/users';
 
-routes.post(commonUrl, UserController.index);
+routes.get(commonUrl, UserController.index);
 
-routes.post(`${commonUrl}/:username/details`, UserDetailsController.index);
+routes.get(`${commonUrl}/:username/details`, UserDetailsController.index);
 
-routes.post(`${commonUrl}/:username/repos`, UserRepositoriesController.index);
+routes.get(`${commonUrl}/:username/repos`, UserRepositoriesController.index);
 
 export default routes;
